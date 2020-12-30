c18ca6c8990933ec272a03d27267d40ea2bbd76a
MistrDev <cwcorster123@gmail.com>
2020-12-30 10:52:38 -0600

    Magic 8 ball!

---
+++ scripts.js
@@ -0,0 +1,40 @@
+// Your name goes here
+const userName = ''
+
+// The question you have for the 8 ball
+let userQuestion = ''
+
+const message = userName ? console.log(`Hello ${userName}`) : console.log('Hello');
+console.log(`${userName} asked: ${userQuestion}`);
+
+const randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = ''
+
+switch(eightBall) {
+  case 0:
+    eightBall = 'It is certain'
+    break;
+  case 1:
+     eightBall = 'It is decidedly so'
+    break;
+  case 2:
+     eightBall = 'Reply hazy try again'
+    break;
+  case 3:
+     eightBall = 'Cannot predict now'
+    break;
+  case 4:
+     eightBall = 'Do not count on it'
+    break;
+  case 5:
+     eightBall = 'My sources say no'
+    break; 
+  case 6:
+     eightBall = 'Outlook not so good'
+    break; 
+  case 7:
+     eightBall = 'Signs point to yes'
+    break; 
+}
+console.log(eightBall);  
